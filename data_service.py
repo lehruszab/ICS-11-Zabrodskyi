4d45d1266fffc45ccf2b76a0a9866cc73362a7f0
Lesha Zabrodskyi <lehruszab@gmail.com>
2020-10-27 15:40:47 +0200

    Виконав лаб. роботу 2, створив data_service.py і створив в ньому get_dovidnik

---
+++ data_service.py
@@ -0,0 +1,13 @@
+def get_dovidnik():
+    from_dovidnik = [
+    "1000;   Тканини;           4",
+    "2000;   Одяг та білизна;   7,5",
+    "3000;   Взуття;            7,5",
+    "4000;   Трикотаж;          7,5",
+    "5000;   Галантерея;        9,5"
+    ]
+
+    return from_dovidnik
+
+dovidnik = get_dovidnik()
+print(dovidnik)
